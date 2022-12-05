ad2c4a8f7033d415bb85ff251b57f7d5839856c6
opti21 <40129778+opti21@users.noreply.github.com>
2022-12-04 22:18:28 -0600

    feat: analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -16,17 +17,30 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,200;0,400;1,400;1,700;1,900&family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
         />
 
-        <link 
+        <link
           rel="preload"
           as="style"
           crossOrigin="anonymous"
-          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" 
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
         />
-        <link 
+        <link
           rel="stylesheet"
           crossOrigin="anonymous"
-          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" 
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-TKRJMBK5WB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-TKRJMBK5WB');
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
